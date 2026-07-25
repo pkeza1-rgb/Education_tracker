55094c977b176f1707703611134712f165cdd1d0
Rebecca <r.isaboke@alustudent.com>
2026-07-25 14:17:18 +0200

    Enable foreign key enforcement in get_connection()

--- database.py
+++ database.py
@@ -9,6 +9,7 @@ DB_NAME = "university.db"
 def get_connection():
     conn = sqlite3.connect(DB_NAME)
     conn.row_factory = sqlite3.Row
+    conn.execute("PRAGMA foreign_keys = ON")
     return conn
 
 def initialize_database():
